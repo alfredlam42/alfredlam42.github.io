4495e86020bc2a241916a0c5ed60bc53b077670b
alfredlam42 <alfred.lam.42@gmail.com>
2015-08-16 18:16:25 -0700

    update website

---
+++ stylesheets/default.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$('.header').mouseenter(function() {
+		$(this).css("background-color", "#FEB729")
+		$(this).css("color", "black")
+	});
+
+	$(".header").mouseleave(function(){
+		$(this).css("background-color", "white")
+		$(this).css("color", "#22A2FF")
+	});
+})
