73ad27246b4db70d0b21e12179bf30a775c67807
alfredlam42 <alfred.lam.42@gmail.com>
2015-08-16 18:39:25 -0700

    fix jquery

--- stylesheets/default.js
+++ stylesheets/default.js
@@ -6,6 +6,6 @@ $(document).ready(function() {
 
 	$(".header").mouseleave(function(){
 		$(this).css("background-color", "white")
-		$(this).css("color", "#22A2FF")
+		$(this).css("color", "#005AB6;")
 	});
 })
